c3a1df25496617945c828b208815f5cf07436e3a
sethbilliau <sethbilliau@gmail.com>
2020-01-31 10:25:41 -0500

    commented cleaner

--- course enrollment.R
+++ course enrollment.R
@@ -2,16 +2,20 @@
 library(tidyverse)
 dat = readxl::read_xlsx("class_enrollment_summary_by_term_1.31.20.xlsx")
 
-# clean 
+### Clean 
+# Removefirst few bad rows
 dat = dat[-c(1:2),]
+
+# recreate header  
 colnames(dat) <- as.character(unlist(dat[1,]))
 dat = dat[-1, ]
+
+# as.numeric char columns 
 dat[7:14] = apply(dat[7:14], 2, as.numeric)
 
-
-# sorting code 
-sort(dat$Total, decreasing = T)
-
+### analyze 
+# sorting by total enrollment
 sorted.df = arrange(dat, desc(Total))
 
-
+# top 10 classes plus the grand total row
+sorted.df[1:11,]
